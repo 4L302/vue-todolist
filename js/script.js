e5d9c4f0008af66882fe6df4f72badbebac15021
4L302 <alegarg2002@gmail.com>
2023-03-31 15:49:14 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,31 +1,57 @@
 /*
-Fare una lista della spesa per Pasquetta, 
-inserire un campo di input dove poter 
-inserire gli elementi della spesa,
-aggiungere un btn per invio elementi,
-per ogni elemento della lista aggiungere un btn canc
-e un btn conferma e un btn con spunta.
+Descrizione:
+Rifare l'esercizio della to do list.
+Ogni todo sarà un oggetto, formato da due proprietà:
+- text, una stringa che indica il testo del todo
+- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+MILESTONE 1
+Stampare all'interno di una lista HTML un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 */
-
 const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            listaSpesa: 
-            [
-                'pane', 'acqua', 'uova', 'latte'
-            ],
+    data() {
+        return {
+            spesaList:
+                [
+                    {
+                        name: 'pane',
+                        completed: false
+                    },
+                    {
+                        name: 'acqua',
+                        completed: false
+                    }
+                ],
             text: ''
 
         }
     },
     methods: {
-        inserisci(){
-            this.listaSpesa.push(this.text)
+        newItem() {
+            const newIngredient = {
+                name: this.ingredient,
+                completed: false
+            }
+            if (this.ingredient != '') this.spesaList.push(newIngredient)
         },
-        deleteList(index){
-            this.lista.splice(index, 1)
+        removeItem() {
+            this.spesaList.splice(index, 1)
+        },
+        completeAction(index) {
+            this.spesaList[index].completed = true
         }
+    },
+    mounted(){
+
     }
+
 }).mount('#app')
